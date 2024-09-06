7534ea7582a2252a54099e96ab2745e9a25eb9df
andrej55556 <102982575+andrej55556@users.noreply.github.com>
2024-09-06 19:02:31 +0500

    Добавьте файлы проекта.

---
+++ PointLibFr/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Общие сведения об этой сборке предоставляются следующим набором
+' набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+' связанные с этой сборкой.
+
+' Проверьте значения атрибутов сборки
+
+<Assembly: AssemblyTitle("PointLibFr")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("PointLibFr")>
+<Assembly: AssemblyCopyright("Copyright ©  2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Указанный ниже идентификатор GUID предназначен для идентификации библиотеки типов, если этот проект будет видимым для COM-объектов
+<Assembly: Guid("d8460bcf-8e47-4d43-966e-3e6d5fee99b6")>
+
+' Сведения о версии сборки состоят из указанных ниже четырех значений:
+'
+'      Основной номер версии
+'      Дополнительный номер версии
+'      Номер сборки
+'      Редакция
+'
+' Можно задать все значения или принять номера сборки и редакции по умолчанию 
+' используя "*", как показано ниже:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
